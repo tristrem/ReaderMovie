a1142a04bcc6bcf61831966f9b99e8af4f43e5f1
Trist <370918304@qq.com>
2017-02-08 18:04:50 +0800

    使用temlpate

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,114 +1,13 @@
+var postData = require("../../data/post-data.js");
 Page({
   data: {
-    text: "This is page data."
-    
-  },
-  onLoad: function(options) {
-    // Do some initialize when page load.
+
   },
   onReady: function() {
-    // Do something when page ready.
-    var post_content = [
-        {
-        authorDate:"Nav 15 2016",
-        authorPic:"/images/post/head1.jpg",
-        contentPic:"/images/post/read1.jpg",
-        contentText:"影片讲述了神盾局指挥官尼克·弗瑞为了对付《雷神》中被流放的洛基，积极奔走寻找最强者，在神盾局斡旋下将钢铁侠、美国队长、雷神托尔、绿巨人、黑寡妇和鹰眼侠六位超级英雄集结在一起，组成了复仇者联盟，共同携手应对邪神洛基。",
-        likePic:"../../images/icon/collect.png",
-        commentPic:"../../images/icon/comment.png",
-        likeCount:"4446",
-        likeCount1:"230"
-        },
-        {
-        authorDate:"Jan 05 2017",
-        authorPic:"/images/post/head2.jpg",
-        contentPic:"/images/post/read2.jpg",
-        contentText:"水墨画，是中国绘画的代表，也就是狭义的“国画”，并传到东亚其他地区。基本的水墨画，仅有水与墨，黑与白色，但进阶的水墨画，也有工笔花鸟画，色彩缤纷，后者有时也称为彩墨画。",
-        likePic:"../../images/icon/collect.png",
-        commentPic:"../../images/icon/comment.png",
-        likeCount:"346",
-        likeCount1:"136"
-        },
-        {
-        authorDate:"Mar 25 2016",
-        authorPic:"/images/post/head3.jpg",
-        contentPic:"/images/post/read3.jpg",
-        contentText:"国际足联年度最佳阵容是由国际足联和国际职业球员联盟颁发的一个奖项，是由超过5.5万名职业球员选出的。前身是国际足联年度最佳阵容（FIFA Team of the Year）和国际职业球员联盟年度最佳阵容（FIFPro World XI）。",
-        likePic:"../../images/icon/collect.png",
-        commentPic:"../../images/icon/comment.png",
-        likeCount:"7746",
-        likeCount1:"823"
-        },
-        {
-        authorDate:"Oct 29 2016",
-        authorPic:"/images/post/head4.jpg",
-        contentPic:"/images/post/read4.jpg",
-        contentText:"小妖怪王大锤（白客饰）法力低微却怀揣着英雄梦，尽管整天都活在别人的质疑声中，他仍自诩为本地妖王。但万万没想到的是，小镇上看似平静的生活突如其来地被搅乱了，在遇到唐僧（陈柏霖饰）师徒后，王大锤的命运也发生了逆转，他和小美（杨子姗饰）帮助丢了法力的孙悟空找回真气，联手与妖怪战斗，开启了一段前所未见的冒险...",
-        likePic:"../../images/icon/collect.png",
-        commentPic:"../../images/icon/comment.png",
-        likeCount:"2746",
-        likeCount1:"323"
-        },
-        {
-        authorDate:"Oct 03 2016",
-        authorPic:"/images/post/head5.jpg",
-        contentPic:"/images/post/read5.jpg",
-        contentText:"爱读书,也爱收集书签。把各式各样的书签夹在书里,不仅是方便,更成了一种艺术美。这里有各式各样的书签,不乏可爱实用,精致唯美。",
-        likePic:"../../images/icon/collect.png",
-        commentPic:"../../images/icon/comment.png",
-        likeCount:"726",
-        likeCount1:"23"
-        },
-        {
-        authorDate:"May 17 2016",
-        authorPic:"/images/post/head6.jpg",
-        contentPic:"/images/post/read6.jpg",
-        contentText:"里约热内卢基督像也叫巴西基督像，是一座装饰艺术风格的大型耶稣基督雕像，位于巴西的里约热内卢基督山上，它是该市的标志，也是世界最闻名的纪念雕塑之一。",
-        likePic:"../../images/icon/collect.png",
-        commentPic:"../../images/icon/comment.png",
-        likeCount:"1226",
-        likeCount1:"83"
-        },
-        {
-        authorDate:"Nov 06 2014",
-        authorPic:"/images/post/head7.jpg",
-        contentPic:"/images/post/read7.jpg",
-        contentText:"世界各国民众戴着电影《V字仇杀队》里主角“V怪客”的面具，参加“百万面具”游行抗议活动。",
-        likePic:"../../images/icon/collect.png",
-        commentPic:"../../images/icon/comment.png",
-        likeCount:"10226",
-        likeCount1:"1283"
-        }
-    ]
+    // Do something when page ready. 
     this.setData({
-      post_content
+      post_contents:postData.postList
     })
-  },
-  onShow: function() {
-    // Do something when page show.
-  },
-  onHide: function() {
-    // Do something when page hide.
-  },
-  onUnload: function() {
-    // Do something when page close.
-  },
-  onPullDownRefresh: function() {
-    // Do something when pull down.
-  },
-  onReachBottom: function() {
-    // Do something when page reach bottom.
-  },
-  onShareAppMessage: function () {
-   // return custom share data when user share.
-  },
-  // Event handler.
-  viewTap: function() {
-    this.setData({
-      text: 'Set some data for updating view.'
-    })
-  },
-  customData: {
-    hi: 'MINA'
   }
+  
 })
